e45c800a37c0141527f8110c7831ffd35772e72f
Sandra Chaparro <sandra.saldepaz@gmail.com>
2026-07-13 20:06:22 -0600

    SEO

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'ProCleaning';
-export const SITE_DESCRIPTION = 'Specialized, efficient, and thorough cleaning services';
+export const SITE_DESCRIPTION = 'Servicios Electricos Residenciales e Industriales en Chihuahua, Mexico.';
 
 
 export const NAV_MENU = [
